9e9ac89e2d9da2f183dd6ba62ac524769820d6d8
oracle <panlizhi@peilian.com>
2020-08-24 11:26:49 +0800

    add source test

---
+++ MKSeriesPop/MKSeriesPopSource/MKSeriesPopManager.h
@@ -0,0 +1,17 @@
+//
+//  MKSeriesPopManager.h
+//  MKSeriesPop
+//
+//  Created by oracle on 2020/8/24.
+//  Copyright © 2020 oracle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MKSeriesPopManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MKSeriesPop/MKSeriesPopSource/MKSeriesPopManager.m
@@ -0,0 +1,13 @@
+//
+//  MKSeriesPopManager.m
+//  MKSeriesPop
+//
+//  Created by oracle on 2020/8/24.
+//  Copyright © 2020 oracle. All rights reserved.
+//
+
+#import "MKSeriesPopManager.h"
+
+@implementation MKSeriesPopManager
+
+@end
